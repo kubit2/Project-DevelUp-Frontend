75f83b9aba8904ec9365e7dd519281b3453a0f2e
희은 <datakubit2@gmail.com>
2024-06-05 17:09:07 +0900

    feat: 해시태그 스크롤바 퍼블리싱

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./App.css";
 import MainPage from "./components/MainPage";
 import SearchBar from "./components/SearchBar";
+import TagsComponent from "./components/TagsComponent";
 
 function App() {
   return (
     <div className="App">
       <SearchBar />
+      <TagsComponent />
       <MainPage />
     </div>
   );
---
+++ src/components/TagsComponent.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./TagsComponent.css";
+
+function TagsComponent() {
+  return (
+    <div className="tags-container">
+      <span className="tag">#Spring</span>
+      <span className="tag">#Nest.js</span>
+      <span className="tag">#DevOps</span>
+      <span className="tag">#JavaScript</span>
+      <span className="tag">#React</span>
+      <span className="tag">#Node</span>
+    </div>
+  );
+}
+
+export default TagsComponent;
